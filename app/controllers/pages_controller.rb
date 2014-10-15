b27c42e07af1fd6d2923145d0c26b9630703afb1
McCartney Bohn <mccartneybohn@yahoo.com>
2014-10-15 13:40:41 -0600

    refractored quotes into the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController <ApplicationController
 
 def index
-end
+  @quotes =[ ["Failure in not an option. Everyone has to succeed","Arnold Schwarzenegger"],
+              ["You're time is limited, so don't waster it living someone else's life","Steve Jobs"],
+              ["Better Ingerdients, Better Pizza","Papa John"] ]
 
+end
 def about
 end
 
