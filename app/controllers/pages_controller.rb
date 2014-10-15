40fabbe44286b0d170c5b0029d4d0964f52d4862
McCartney Bohn <mccartneybohn@yahoo.com>
2014-10-15 14:20:54 -0600

    rearranged coding

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,21 @@
 class PagesController <ApplicationController
 
 def index
-  @quotes =[ ["Failure in not an option. Everyone has to succeed","Arnold Schwarzenegger"],
-              ["You're time is limited, so don't waster it living someone else's life","Steve Jobs"],
-              ["Better Ingerdients, Better Pizza","Papa John"] ]
+
+
+  quote1 = Quote.new
+  quote1.text = "Failure is not an option. Everyone has to succeed"
+  quote1.author = "Arnold Schwarzenegger"
+
+  quote2 = Quote.new
+  quote2.text = "You're time is limited, so don't waster it living someone else's life"
+  quote2.author = "Steve Jobs"
+
+  quote3 = Quote.new
+  quote3.text = "Better Ingerdients, Better Pizza"
+  quote3.author = "Papa John"
+  
+  @quotes =[quote1, quote2, quote3]
 
 end
 def about
