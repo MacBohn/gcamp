e3e4899364dfa87d6e027525b8a99e42cc6939e3
McCartney Bohn <mccartneybohn@yahoo.com>
2014-10-17 15:18:27 -0600

    ran tasks migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :tasks
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-root "pages#index"
+  root "pages#index"
 
   get "/about" => "pages#about", as: :about
   get "/terms" => "pages#terms", as: :terms
