c9e0a6ef8e531ba9d5803c9aa424330d32cfa985
McCartney Bohn <mccartneybohn@yahoo.com>
2014-10-28 16:14:15 -0600

    added update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,13 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    @user.save
+    redirect_to users_path, notice: 'User was successfully updated'
+  end
+  
   def create
     @user = User.new(user_params)
     @user.save
