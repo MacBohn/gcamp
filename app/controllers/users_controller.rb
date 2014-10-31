a1e7164494d16b9e4114ee8323a9342fa28a49c7
McCartney Bohn <mccartneybohn@yahoo.com>
2014-10-31 09:55:51 -0600

    heroku run db migrate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,9 @@ class UsersController < ApplicationController
     @user.save
     redirect_to users_path, notice: 'User was successfully created.'
   end
+  def show
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name , :email)
