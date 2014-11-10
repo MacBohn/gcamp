080d5f77fd016ff422c2c075dc1817d16704b9a2
McCartney Bohn <mccartneybohn@yahoo.com>
2014-11-10 15:01:01 -0700

    added errors to the edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,10 +24,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
+    if @user.save
     redirect_to users_path, notice: 'User was successfully created.'
+  else
+    render :new
   end
-
+end
   def show
     @user = User.find(params[:id])
   end
--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -2,13 +2,27 @@ require 'rails_helper'
 
   feature "Sign In" do
     scenario "Sign In" do
-      User.create!(email:"yo@yo.com", password:"yo")
+      User.create!(first_name: "calo",
+       last_name: "yoyoy",
+       email:"yo@yo.com",
+        password:"yo")
       visit sign_in_path
       click_on "Sign In"
       fill_in "Email", with: "yo@yo.com"
       fill_in "Password", with: "yo"
+      fill_in "Password confirmation", with: "yo"
       click_on "Sign in"
       expect(page).to have_content("© Mega Mac Company")
       save_and_open_page
      end
+    #  scenario "Sign In" do
+    #    User.create!(email:"yo@yo.com", password:"yo")
+    #    visit sign_in_path
+    #    click_on "Sign In"
+    #    fill_in "Email", with: "yo@yo.com"
+    #    fill_in "Password", with: "yo"
+    #    click_on "Sign in"
+    #    expect(page).to have_content("© Mega Mac Company")
+    #    save_and_open_page
+    #   end
    end
